8cd28e50f093b26cb7ecdcc175ef66de8b6142b8
hiroygo <pcxn1a@gmail.com>
2020-06-10 23:44:37 +0900

    add 通信部分を実装

--- cmd/main.go
+++ cmd/main.go
@@ -2,42 +2,25 @@ package main
 
 import (
 	"errors"
+	"flag"
 	"fmt"
 	"goping/internal"
 	"net"
 	"time"
 )
 
-func readn(conn net.Conn, timeout time.Duration, n int) ([]byte, error) {
-	if n < 1 {
-		msg := fmt.Sprintf("readn error:%d は不正です。", n)
+func getIPv4Addr(ipv4 string) (*net.IPAddr, error) {
+	ip := net.ParseIP(ipv4)
+	if ip == nil {
+		msg := fmt.Sprintf("getIPv4Addr error:%v をパースできません。", ipv4)
 		return nil, errors.New(msg)
 	}
 
-	var results []byte
-	start := time.Now()
-	for {
-		var buff []byte
-		readLen, err := conn.Read(buff)
-		if err != nil {
-			msg := fmt.Sprintf("readn error:%v", err)
-			return nil, errors.New(msg)
-		}
-
-		if time.Since(start) >= timeout {
-			return nil, errors.New("readn error:timeout")
-		}
-
-		results = append(results, buff...)
-		n -= readLen
-		if n == 0 {
-			return results, nil
-		}
-	}
+	return &net.IPAddr{IP: ip}, nil
 }
 
-func ping(destIP string, timeout time.Duration, identifier uint16, sequenceNumber uint16, dataBytes uint16) (time.Duration, error) {
-	// ペイロードはすべて 0 になる
+func ping(remoteIP string, timeout time.Duration, identifier uint16, sequenceNumber uint16, dataBytes uint16) (time.Duration, error) {
+	// ペイロードはすべて 0 で作成する
 	echoRequest := internal.NewEchoRequest(identifier, sequenceNumber, make([]byte, dataBytes))
 	writeBytes, err := internal.MarshalEcho(echoRequest)
 	if err != nil {
@@ -45,14 +28,26 @@ func ping(destIP string, timeout time.Duration, identifier uint16, sequenceNumbe
 		return time.Duration(0), errors.New(msg)
 	}
 
-	// 接続
-	conn, err := net.DialTimeout("ip4:1", destIP, timeout)
+	// 接続先
+	remoteAddr, err := getIPv4Addr(remoteIP)
 	if err != nil {
 		msg := fmt.Sprintf("ping error:%v", err)
 		return time.Duration(0), errors.New(msg)
 	}
 
-	// Write, Read のタイムアウトを設定する
+	/*
+		ソケットを生成する
+		IP なので connect するわけではない
+		ただし送信先情報は保存されるため WriteTo する必要はない
+	*/
+	conn, err := net.DialIP("ip4:icmp", nil, remoteAddr)
+	if err != nil {
+		msg := fmt.Sprintf("ping error:%v", err)
+		return time.Duration(0), errors.New(msg)
+	}
+	defer conn.Close()
+
+	// Write, ReadFrom のタイムアウトを設定する
 	err = conn.SetDeadline(time.Now().Add(timeout))
 	if err != nil {
 		msg := fmt.Sprintf("ping error:%v", err)
@@ -60,25 +55,31 @@ func ping(destIP string, timeout time.Duration, identifier uint16, sequenceNumbe
 	}
 
 	// 送信
-	writeStart := time.Now()
+	request := time.Now()
 	{
-		wroteLen, err := conn.Write(writeBytes)
+		// WriteTo でもいいけど、DialIP で送信先を設定してるので Write を使う
+		_, err := conn.Write(writeBytes)
 		if err != nil {
 			msg := fmt.Sprintf("ping error:%v", err)
 			return time.Duration(0), errors.New(msg)
 		}
-		if wroteLen != len(writeBytes) {
-			return time.Duration(0), errors.New("ping error:1 回の Write で全てを送信できませんでした。")
-		}
 	}
 
 	// 受信
-	readBytes, err := readn(conn, timeout, len(writeBytes))
+	readBytes := make([]byte, 1024)
+	readSize, fromIP, err := conn.ReadFrom(readBytes)
+	reply := time.Now()
 	if err != nil {
 		msg := fmt.Sprintf("ping error:%v", err)
 		return time.Duration(0), errors.New(msg)
 	}
-	readEnd := time.Now()
+	if fromIP.String() != remoteAddr.String() {
+		msg := fmt.Sprintf("ping error:パケット送信元 %v が EchoRequest 先 %v と異なります。", fromIP, remoteAddr)
+		return time.Duration(0), errors.New(msg)
+	}
+
+	// バッファをリサイズする
+	readBytes = readBytes[:readSize]
 
 	// 受信データを構造体にする
 	echoReply := &internal.ICMPEchoMessage{}
@@ -91,15 +92,27 @@ func ping(destIP string, timeout time.Duration, identifier uint16, sequenceNumbe
 		return time.Duration(0), errors.New("ping error:EchoReply のフィールドが一致しません。")
 	}
 
-	return readEnd.Sub(writeStart), nil
+	return reply.Sub(request), nil
 }
 
 func main() {
-	for i := 0; i < 5; i++ {
-		if duration, err := ping("localhost", time.Second*5, uint16(i), uint16(i), 32); err != nil {
+	flag.Parse()
+	args := flag.Args()
+	if len(args) == 0 {
+		fmt.Println("引数に宛先 ipv4 アドレスを入力してください。")
+		return
+	}
+
+	fmt.Printf("Pinging %v\n", args[0])
+
+	// FIXME:i == 0 のとき、宛先に関係なく "ping error:UnmarshalEcho error:チェックサム 65535 は再計算で 0x0000 になりません。" が発生する。
+	for i := 1; i < 5; i++ {
+		if duration, err := ping(args[0], time.Second*5, uint16(i), uint16(i), 32); err != nil {
 			fmt.Printf("%v\n", err)
 		} else {
-			fmt.Printf("TimeDuration:%v\n", duration)
+			fmt.Printf("返答を受信:RTT=%v ms\n", duration.Milliseconds())
 		}
+
+		time.Sleep(time.Second)
 	}
 }
